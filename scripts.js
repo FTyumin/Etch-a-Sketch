c966bd55871bc8ea85db4cf45446b905bcf16b45
FTyumin <feodor.tjumin.28@gmail.com>
2024-11-06 20:56:00 +0200

    feat:opacity feature

--- scripts.js
+++ scripts.js
@@ -1,25 +1,53 @@
 let container = document.querySelector(".container");
 
-for(let i=0;i<4;i++) {
-    let div = document.createElement('div');
-    div.setAttribute("id", "main")
-    for(let j=0;j<4;j++) {
-        let innerDiv = document.createElement('div');
-        innerDiv.addEventListener("mouseover", () => {
-            console.log("Event triggered");
-            innerDiv.style.background = "red";
-        })
-        innerDiv.setAttribute("id", "grid");
-        div.appendChild(innerDiv);
-    }
+let i=10;
+function value() {
+    if(i==0) {i=10;}
+    opacity = i/10;
+    i--;
+    console.log(opacity);
+    return opacity;
+};
 
-    container.appendChild(div);
+
+function createGrid(sideLength) {
+    for(let i=0;i<sideLength;i++) {
+        let div = document.createElement('div');
+        div.setAttribute("id", "main")
+        for(let j=0;j<sideLength;j++) {
+            let innerDiv = document.createElement('div');
+
+            innerDiv.addEventListener("mouseover", () => {
+                // generating random color, code from css tricks
+                let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            console.log("Event triggered");
+            let color = '#' + randomColor;
+            
+            innerDiv.style.background = color;
+            
+            innerDiv.style.opacity =value();
+            })
+            innerDiv.setAttribute("class", "grid");
+            div.appendChild(innerDiv);
+        }
+    
+        container.appendChild(div);
+    }
 }
 
-// let gridElements = document.querySelectorAll("#grid");
-// for(element of gridElements) {
-//     element.addEventListener("mouseover", () => {
-//         console.log("Event triggered");
-//         element.style.background = "red";
-//     })
-// }
+function clearGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+createGrid(4);
+
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+
+    let sideLength = prompt("Enter amount of squares for grid", 4);
+    clearGrid();
+    createGrid(sideLength);
+})
+

a16727ebc88e4730b2809db5f32ed78db001393a
FTyumin <feodor.tjumin.28@gmail.com>
2024-11-06 15:13:40 +0200

    feat:grid mouseover eventListener

--- scripts.js
+++ scripts.js
@@ -5,8 +5,21 @@ for(let i=0;i<4;i++) {
     div.setAttribute("id", "main")
     for(let j=0;j<4;j++) {
         let innerDiv = document.createElement('div');
+        innerDiv.addEventListener("mouseover", () => {
+            console.log("Event triggered");
+            innerDiv.style.background = "red";
+        })
+        innerDiv.setAttribute("id", "grid");
         div.appendChild(innerDiv);
     }
 
     container.appendChild(div);
 }
+
+// let gridElements = document.querySelectorAll("#grid");
+// for(element of gridElements) {
+//     element.addEventListener("mouseover", () => {
+//         console.log("Event triggered");
+//         element.style.background = "red";
+//     })
+// }
